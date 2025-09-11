59cc40fc1e841a66fb4c9392612e8986eec54b3e
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-11 16:58:22 +0530

    Update provider.tf
    updated region in providr.tf file

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.12.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  # Configuration options
+  region = "uap-south-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }

0a8e42aa808ece329a712bf5ee6a3447a62d01d9
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-13 21:27:29 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "tpni-1" {
-  vpc_id     = aws_vpc.tpni-1.id
+  vpc_id     = aws_vpc.tpni.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -7,15 +7,15 @@ resource "aws_subnet" "tpni-1" {
   }
 }
 resource "aws_subnet" "tpni-2" {
-  vpc_id     = aws_vpc.tpni-1.id
+  vpc_id     = aws_vpc.tpni.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "tpni-2"
   }
 }
-resource "aws_subnet" "tpni-3" {
-  vpc_id     = aws_vpc.tpni-1.id
+esource "aws_subnet" "tpni-3" {
+  vpc_id     = aws_vpc.tpni.id
   cidr_block = "10.0.3.0/24"
 
   tags = {

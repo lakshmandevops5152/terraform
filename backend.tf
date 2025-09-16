0f03a394ddae8d7af3cc37331ba7b911ef8c5aa7
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-16 18:41:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "telstra-terraform"
+    bucket         = "lakshman-trraform"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true

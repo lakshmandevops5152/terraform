4e114b13dfeba563bb8a362be2164fb70f1676be
lakshmandevops5152 <malapatilakshman@gmail.com>
2025-09-26 16:18:20 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.39.0"
-    }
-  }
-}
 
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
-  profile = "terraform"
-}
